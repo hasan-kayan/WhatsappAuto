f2a72dcf0298c736a2f53a306a4aaee896f3f56c
hasan-kayan <hasankayan2000@hotmail.com>
2022-12-01 22:23:16 +0300

    This program will be a Whatsapp Automation which will work on a RusberyPi

---
+++ WhatsappChatSel.py
@@ -0,0 +1,9 @@
+#WhatsappChatBoot 
+import pywhatkit as kit
+import pyautogui as auto 
+import selenium as sel
+import time 
+
+
+# This application will take all of the massages and will response if its neccesary 
+# NOT group will be the opparator from out of the computer and will take order from the group 
---
+++ autoGUI.py
@@ -0,0 +1,13 @@
+import pywhatkit as kit 
+import pyautogui as auto 
+import selenium as sel 
+from selenium import webdriver 
+import time
+
+
+time.sleep(5) 
+
+for i in range(200): 
+    auto.typewrite("NABERRRRR ???? ") 
+    auto.press("enter") 
+    time.sleep(1) 
